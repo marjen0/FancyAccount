e6da4efd7fd5a91aeb41ae438bb8d44c6f6d789d
marjen0 <m.jenulis@gmail.com>
2021-12-15 14:42:32 +0200

    feat: add splash screens

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {TextInput as RNTextInput, TextInputProps} from 'react-native';
 
 export interface ITextInputProps extends TextInputProps {}
-
+console.log('a');
 const TextInput = (props: ITextInputProps) => {
   return <RNTextInput {...props} />;
 };
